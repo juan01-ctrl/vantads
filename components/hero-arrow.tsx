6f1bf46e27a9d4898028b6037e176e3656965a88
Juan Ierace <juanierace2@gmail.com>
2026-08-06 15:05:13 -0300

    feat: hero arrow, money-back guarantee, premium pricing hover

---
+++ components/hero-arrow.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { ArrowIcon } from "./icons";
+
+export function HeroArrow() {
+  return (
+    <div className="hero-arrow absolute right-6 top-32 text-white sm:right-10 sm:top-36">
+      <ArrowIcon className="h-8 w-8" />
+    </div>
+  );
+}
